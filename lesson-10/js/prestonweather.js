42578ef5bc4c6300136ac946ce0a35083b031659
edsonescalante <esc18001@byui.edu>
2021-11-18 18:46:03 -0700

    commit

--- lesson-10/js/prestonweather.js
+++ lesson-10/js/prestonweather.js
@@ -1,4 +1,4 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=be76dc31a6e1904015882fc9237649b1";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=be76dc31a6e1904015882fc9237649b1";
 
 fetch(apiURL)
     .then((response) => response.json())
